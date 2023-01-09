2be064ae9d41f68cb8f563e8972f2d617b25fbcd
Razvan Bunea <razvan.bunea@orange.com>
2023-01-09 15:20:30 +0100

    feat: add support for Kotlin Android sourceSets

--- src/main/kotlin/JacocoToCoberturaPlugin.kt
+++ src/main/kotlin/JacocoToCoberturaPlugin.kt
@@ -1,5 +1,6 @@
 package net.razvan
 
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFileProperty
@@ -9,7 +10,11 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.SourceSet
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.simpleframework.xml.Serializer
 import org.simpleframework.xml.core.Persister
 import org.simpleframework.xml.stream.Format
@@ -28,45 +33,52 @@ class JacocoToCoberturaPlugin : Plugin<Project> {
     }
 
     private fun convert(project: Project, extension: JacocoToCoberturaExtension) {
-        log(extension,"Conversion jacoco report to cobertura")
+        log(extension, "Conversion jacoco report to cobertura")
         try {
             val input = extension.inputFile.get().asFile
-                    .also {
-                        if (!it.exists()) throw JacocoToCoberturaException("File `${it.canonicalPath}` does not exists (current dir: `${File(".").canonicalPath})`")
-                    }
+                .also {
+                    if (!it.exists()) throw JacocoToCoberturaException(
+                        "File `${it.canonicalPath}` does not exists (current dir: `${
+                            File(
+                                "."
+                            ).canonicalPath
+                        })`"
+                    )
+                }
             val output = extension.outputFile.orNull?.asFile ?: defaultOutputFile(input)
             makeDirsIfNeeded(output.parentFile)
 
             val splitByPackage = extension.splitByPackage.getOrElse(false)
 
-            log(extension,"\tJacocoToCobertura: Calculated configuration: input: $input, output: $output, splitByPackage: $splitByPackage")
+            log(
+                extension,
+                "\tJacocoToCobertura: Calculated configuration: input: $input, output: $output, splitByPackage: $splitByPackage"
+            )
             val customSourcesConf = emptySet<File>()
 
             val jacocoData = loadJacocoData(input)
 
-            val kotlinSourcesSet = mutableSetOf<File>()
-            val kotlinSources = kotlin.runCatching {
-                project.extensions.getByType(KotlinMultiplatformExtension::class.java)
-                        .sourceSets
-                        .filterNot { it.name.contains("test", true) }
-                        .forEach { sourcesSet ->
-                            sourcesSet.kotlin.srcDirs.forEach { file ->
-                                file.walkTopDown().forEach {
-                                    if (it.isFile) kotlinSourcesSet.add(it)
-                                }
-                            }
-                        }
-                kotlinSourcesSet
-            }.getOrNull() ?: kotlinSourcesSet
-            val javaSources = kotlin.runCatching { project.extensions.getByType(JavaPluginExtension::class.java).sourceSets }.getOrNull()
-                    ?.findByName(SourceSet.MAIN_SOURCE_SET_NAME)?.allSource?.files ?: emptySet()
-            val customSources = customSourcesConf
-                    .filter { it.exists() }
-                    .mapNotNull { it.listFiles()?.toList() }
-                    .flatten()
-                    .toSet()
-            val allSources = kotlinSources + javaSources + customSources
+            val kotlinSources: Set<File> = allKotlinSources(project)
 
+            val javaSources = runCatching { project.extensions.getByType(JavaPluginExtension::class.java).sourceSets }
+                .getOrNull()
+                ?.findByName(SourceSet.MAIN_SOURCE_SET_NAME)?.allSource?.files
+                ?.filter { it !in kotlinSources }
+                ?: emptySet()
+            val customSources = customSourcesConf
+                .filter { it.exists() }
+                .mapNotNull { it.listFiles()?.toList() }
+                .flatten()
+                .toSet()
+
+            log(extension, "Sources: - Kotlin: `${kotlinSources.size}`, Java: `${javaSources.size}`")
+            val allSources = kotlinSources + javaSources + customSources
+            kotlinSources.forEach {
+                println("Kotlin - ${it.canonicalPath}")
+            }
+            javaSources.forEach {
+                println("Java - ${it.canonicalPath}")
+            }
             val roots = sourcesRoots(allSources, jacocoData)
 
             if (splitByPackage) {
@@ -80,7 +92,7 @@ class JacocoToCoberturaPlugin : Plugin<Project> {
                 writeCoberturaData(output, transformData(jacocoData, roots))
             }
 
-            log(extension,"Cobertura report generated in: `$output`")
+            log(extension, "Cobertura report generated in: `$output`")
         } catch (e: Exception) {
             println("Error while running JacocoToCobertura conversion: `${e.message} [configuration: input: `${extension.inputFile.get()}` | output: `${extension.outputFile.get()}`]")
             if (e !is JacocoToCoberturaException) {
@@ -89,11 +101,39 @@ class JacocoToCoberturaPlugin : Plugin<Project> {
         }
     }
 
+    private fun allKotlinSources(project: Project): Set<File> {
+        val multi = runCatching {
+            kotlinSourcesFiles(project.extensions.getByType(KotlinMultiplatformExtension::class.java).sourceSets)
+        }.getOrDefault(emptySet())
+        val android = runCatching {
+            kotlinSourcesFiles(project.extensions.getByType(KotlinAndroidProjectExtension::class.java).sourceSets)
+        }.getOrDefault(emptySet())
+        val jvm = runCatching {
+            kotlinSourcesFiles(project.extensions.getByType(KotlinJvmProjectExtension::class.java).sourceSets)
+        }.getOrDefault(emptySet())
+        val js = runCatching {
+            kotlinSourcesFiles(project.extensions.getByType(KotlinJsProjectExtension::class.java).sourceSets)
+        }.getOrDefault(emptySet())
+
+        return multi + android + jvm + js
+    }
+
+    private fun kotlinSourcesFiles(sourceSets: NamedDomainObjectContainer<KotlinSourceSet>): Set<File> =
+        sourceSets
+            .filterNot { it.name.contains("test", true) }
+            .flatMap { sourcesSet ->
+                sourcesSet.kotlin.srcDirs.flatMap { file ->
+                    file.walkTopDown().mapNotNull {
+                        if (it.isFile) it else null
+                    }
+                }
+            }.toSet()
+
     @Throws(JacocoToCoberturaException::class)
     private fun makeDirsIfNeeded(file: File?) {
-        if(file != null && !file.exists()) {
+        if (file != null && !file.exists()) {
             try {
-                if (!file.mkdirs()) throw JacocoToCoberturaException("`mkdirs()` return false")
+                if (!file.mkdirs()) throw JacocoToCoberturaException("`mkdirs()` returned false")
             } catch (e: Exception) {
                 throw JacocoToCoberturaException("Output file directory `${file.canonicalPath} does not exists and couldn't be created, error: `${e.message}`")
             }
@@ -136,14 +176,21 @@ class JacocoToCoberturaPlugin : Plugin<Project> {
     private fun sourcesRoots(allSources: Set<File>, jacocoData: Jacoco.Report): Set<String> {
         val jacocoPackages = jacocoData.packagesNames()
         return allSources.map { it.canonicalPath }
-                .mapNotNull { sourceName ->
-                    val p = jacocoPackages.firstOrNull { sourceName.contains(it) }
-                            ?: jacocoPackages.firstOrNull { sourceName.contains(it.replace("/", ".")) } // in case the package is in dot format
-                    p?.let {
-                        val sourcePath = sourceName.substringBefore(it)
-                        if (sourcePath != sourceName) sourcePath else sourceName.substringBefore(it.replace("/", "."))
-                    }
-                }.toSet()
+            .mapNotNull { sourceName ->
+                val p = jacocoPackages.firstOrNull { sourceName.contains(it) }
+                    ?: jacocoPackages.firstOrNull {
+                        sourceName.contains(
+                            it.replace(
+                                "/",
+                                "."
+                            )
+                        )
+                    } // in case the package is in dot format
+                p?.let {
+                    val sourcePath = sourceName.substringBefore(it)
+                    if (sourcePath != sourceName) sourcePath else sourceName.substringBefore(it.replace("/", "."))
+                }
+            }.toSet()
     }
 
     fun log(extension: JacocoToCoberturaExtension, msg: String) {
