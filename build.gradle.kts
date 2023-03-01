b636bddc982a73fe5473952b547b2b65538e2b8d
Christopher-Marcel Esser <chris.esser.91@gmail.com>
2023-03-01 10:18:40 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java-gradle-plugin")
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
     id("maven-publish")
     id("com.gradle.plugin-publish") version "1.1.0"
 }
@@ -15,9 +15,9 @@ repositories {
 
 dependencies {
     implementation("org.simpleframework:simple-xml:2.7.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
@@ -31,20 +31,17 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
-pluginBundle {
-    website = "https://github.com/razvn/jacoco-to-cobertura-gradle-plugin"
-    vcsUrl = "https://github.com/razvn/jacoco-to-cobertura-gradle-plugin"
-    tags = listOf("jacoco", "cobertura", "report", "converter")
-}
-
 gradlePlugin {
+    website.set("https://github.com/razvn/jacoco-to-cobertura-gradle-plugin")
+    vcsUrl.set(website)
+
     plugins {
         create("JacocoToCoberturaPlugin") {
             id = "net.razvan.jacoco-to-cobertura"
             displayName = "Converts jacoco xml reports to cobertura"
             description = "This plugin converts jacoco xml reports to cobertura. Make sure the `jacocoTestReport` runs before."
+            tags.set(listOf("jacoco", "cobertura", "report", "converter"))
             implementationClass = "net.razvan.JacocoToCoberturaPlugin"
         }
     }
 }
-
